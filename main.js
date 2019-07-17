862df33b48e944a4f035a0246476f3b85150226a
enrico76 <enricodip@gmail.com>
2019-07-17 18:21:05 +0200

    click on green square jquery added

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+// Creare una griglia formata da 8x8 quadratini tutti bianchi.
+// 15 di questi quadratini (scelti a caso all’inizio)
+//  se cliccati diventano rossi, gli altri diventano verdi
 console.log("inizio script");
+
+$(document).ready(function(){
+  $(".quadrato").click(function(){
+    $(this).toggleClass("greencolor");
+  });
+});
